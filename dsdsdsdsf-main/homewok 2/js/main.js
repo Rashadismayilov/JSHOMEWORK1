6d77eab364dc520c1f85d532f91526e8f3d832af
Rashadismayilov <112488764+Rashadismayilov@users.noreply.github.com>
2022-11-22 19:46:01 +0400

    Add files via upload

---
+++ dsdsdsdsf-main/homewok 2/js/main.js
@@ -0,0 +1,41 @@
+//Task2
+
+// function myFunction(){
+//     let person = prompt('Please enter your age')
+//     if(person<18){
+//        alert('Yasin catmir') 
+//     }else
+//     {}
+// }
+
+//Task1
+
+ function myFunction() {
+     confirm("Press a button!")   
+     document.getElementById("demo").style.display = 'block'
+  }
+
+//Task3
+
+// function group(){
+
+//     let n= document.getElementById('inp').value 
+//     if(n.length>=4){
+//         let chn = n.slice(-3,-2)
+//         if(chn ==1) {
+//             alert(`Xosgeldiniz ${n}`)
+//             document.body.style.backgroundColor = 'red'
+//         }
+//         else if(chn==2){
+//             alert(`Xosgeldiniz ${n}`)
+//             document.body.style.backgroundColor = 'yellow'
+//         }
+//         else if(chn==3){
+//             alert(`Xosgeldiniz ${n}`)
+//             document.body.style.backgroundColor = 'black'
+//         }
+//         else{
+//             alert('Duzgun daxil edin')
+//         }
+//     }
+// }
